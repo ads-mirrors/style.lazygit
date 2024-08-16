a464aa9f9fa38984ad52cb31c3cb249571df259a
Stefan Haller <stefan@haller-berlin.de>
2024-08-16 11:32:52 +0200

    Use our new hyperlink support in the information view

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -785,7 +785,6 @@ type TranslationSet struct {
 	MarkAsBaseCommitTooltip                  string
 	MarkedCommitMarker                       string
 	FailedToOpenURL                          string
-	PleaseGoToURL                            string
 	NoCopiedCommits                          string
 	DisabledMenuItemPrefix                   string
 	QuickStartInteractiveRebase              string
@@ -1772,7 +1771,6 @@ func EnglishTranslationSet() *TranslationSet {
 		MarkAsBaseCommitTooltip:                  "Select a base commit for the next rebase. When you rebase onto a branch, only commits above the base commit will be brought across. This uses the `git rebase --onto` command.",
 		MarkedCommitMarker:                       "↑↑↑ Will rebase from here ↑↑↑",
 		FailedToOpenURL:                          "Failed to open URL %s\n\nError: %v",
-		PleaseGoToURL:                            "Please go to {{.url}}",
 		DisabledMenuItemPrefix:                   "Disabled: ",
 		NoCopiedCommits:                          "No copied commits",
 		QuickStartInteractiveRebase:              "Start interactive rebase",
