29f6b17a56e920e640b894bcc1aaa3630e7f2ccf
Stefan Haller <stefan@haller-berlin.de>
2025-02-15 12:43:38 +0100

    Add a "Copy to clipboard" menu to the Commit Files panel
    This is very similar to the same menu in the Files panel, except that it works
    on whatever diff is currently shown in the main view, including range diffs
    either in diffing mode (shift-W), or from a range selection of commits.

--- pkg/integration/tests/test_list.go
+++ pkg/integration/tests/test_list.go
@@ -164,6 +164,7 @@ var tests = []*components.IntegrationTest{
 	demo.StageLines,
 	demo.Undo,
 	demo.WorktreeCreateFromBranches,
+	diff.CopyToClipboard,
 	diff.Diff,
 	diff.DiffAndApplyPatch,
 	diff.DiffCommits,
