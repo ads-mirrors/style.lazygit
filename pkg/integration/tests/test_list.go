susl/lazygit

Add new command "Move commits to new branch"

pkg/integration/tests/test_list.go
@@ -43,6 +43,8 @@ var tests = []*components.IntegrationTest{
 	branch.Delete,
 	branch.DeleteRemoteBranchWithCredentialPrompt,
 	branch.DetachedHead,
+	branch.MoveCommitsToNewBranch,
+	branch.MoveCommitsToNewBranchNoUpstream,
 	branch.NewBranchFromRemoteTrackingDifferentName,
 	branch.NewBranchFromRemoteTrackingSameName,
 	branch.OpenPullRequestNoUpstream,

ads-mirrors/style.lazygit

pkg/integration/tests/test_list.go
@@ -47,6 +47,9 @@ var tests = []*components.IntegrationTest{
 	branch.DeleteRemoteBranchWithDifferentName,
 	branch.DeleteWhileFiltering,
 	branch.DetachedHead,
+	branch.MoveCommitsToNewBranchFromBaseBranch,
+	branch.MoveCommitsToNewBranchFromMainBranch,
+	branch.MoveCommitsToNewBranchKeepStacked,
 	branch.NewBranchAutostash,
 	branch.NewBranchFromRemoteTrackingDifferentName,
 	branch.NewBranchFromRemoteTrackingSameName,